87a6b5f77315eeee0a3c8f759ab501929635ef14
Pietro Vitagliano <73847820+pietrovitagliano@users.noreply.github.com>
2023-10-11 14:02:11 +0200

    Add files via upload

---
+++ test/ltc_2499_adc_test.h
@@ -0,0 +1,18 @@
+/*
+ * ltc_2499_adc.h
+ *
+ * Header file for the API testing of the LTC 2499 ADC.
+ *
+ * Author: Pietro Vitagliano
+ */
+
+#ifndef LTC_2499_ADC_TEST_H
+#define LTC_2499_ADC_TEST_H
+
+#include "sys_app.h"
+
+
+// Function used to test the API
+int32_t api_test();
+
+#endif /* LTC_2499_ADC_TEST_H */
